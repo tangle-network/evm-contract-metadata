be408aa98e273f17e85eba69d6edc8ea2b416d39
Pavan Soratur <pavansoratur@MacBookAir.lan>
2024-11-06 10:55:19 -0800

    chore: update workflows

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-export { EVMTokens } from './evm/tokens';
-export { EVMChainEnum, EVMTokenEnum } from './evm/types';
-export { HyperlaneAddresses } from './hyperlane/addresses';
-export { HyperlaneChains } from './hyperlane/chains';
-export type { HyperlaneAddressesType } from './hyperlane/types';
-export { HyperlaneChainEnum } from './hyperlane/types';
-export {
-  HyperlaneWarpRouteConfig,
-  HyperlaneWarpRouteWhitelist
-} from './hyperlane/warpRoutes';
+export * from './evm';
+export * from './hyperlane';
